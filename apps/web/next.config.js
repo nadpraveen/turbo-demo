1e21b149c154eef3cdee513ef3aba601aa01b889
Praveen Kumar <praveen.42000@gmail.com>
2023-03-27 21:13:52 +0530

    next js config updated

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  output: {
+    // Set the path where the standalone files will be generated
+    path: `${__dirname}/dist`,
+    // Set the public path of your app
+    publicPath: '/',
+  },
+  env: {
+    NEXT_PRIVATE_STANDALONE: process.env.NEXT_PRIVATE_STANDALONE,
+  },
 };
