d49343ed2ddaf5bf45165916a6b8a6943a6de86a
Praveen Kumar <praveen.42000@gmail.com>
2023-03-28 12:22:11 +0530

    next js config updated

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-  output: {
-    // Set the path where the standalone files will be generated
-    path: `${__dirname}/.next/standalone`,
-    // Set the public path of your app
-    publicPath: '/',
-  },
+  output: "standalone",
   async headers() {
     return [
       {
-        // Set the X-Frame-Options header to prevent clickjacking
         source: '/',
         headers: [
           {
